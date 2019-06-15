4dda6bc01b5056090176448734e83f27ca7b8af0
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-06-15 20:29:10 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,12 @@ def my_select(collection)
   other_collection = []
   i = 0 
   while i < collection.length do
-    
+    if yield (collection[i]) == true
+  other_collection << collection 
+end 
+i += 1 
+end 
+other_collection
   
     
 end
