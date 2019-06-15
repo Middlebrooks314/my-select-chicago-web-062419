d5b1ae23e0cf163dff2526af893ce53df5c68822
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-06-15 20:30:41 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -11,20 +11,3 @@ other_collection
   
     
 end
-
-
-=begin 
-
-if yield(array[count]) == true
-cool_nums = [1, 2, 3, 4, 5]
- 
-def even_nums(nums)
-  nums.select do |x|
-    x.even?
-  end
-end
- 
-even_nums(cool_nums)
-#=> [2, 4]
-
-end= 
