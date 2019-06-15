70d4198396b4a4f9927790d770a7d8aa6ccb2d36
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-06-15 19:45:17 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,20 @@
 def my_select(collection)
- # your code here!
+ collection.select do |x|
+   x.even?
+  end 
 end
+
+
+=begin 
+cool_nums = [1, 2, 3, 4, 5]
+ 
+def even_nums(nums)
+  nums.select do |x|
+    x.even?
+  end
+end
+ 
+even_nums(cool_nums)
+#=> [2, 4]
+
+end= 
